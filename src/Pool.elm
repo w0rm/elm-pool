0e3eb1885f89e121b75ea996295da3b8ee5e35b9
Jared M. Smith <absynce@gmail.com>
2020-11-12 15:44:26 -0600

    Start implementing game logic using phantom types
    Next step: handle when cue strikes cue ball, but no collisions occur.

--- src/Pool.elm
+++ src/Pool.elm
@@ -1,7 +1,13 @@
 module Pool exposing
-    ( Pool, eightBall
-    , Action(..), update, nextAction
-    , Ball(..)
+    ( Pool, start
+    , rack, ballPlacedInKitchen, playerShot
+    , cueHitBall
+    , oneBall
+    , WhatHappened(..)
+    -- , Action(..)
+    -- , update, nextAction
+    -- , currentPlayer, balls
+    -- , eightBall
     )
 
 {-| Pool game rules. Agnostic to game engine.
@@ -9,59 +15,116 @@ module Pool exposing
 Ideas:
 
   - `Valid` for enforcing safe state
+
   - Use phantom type to enforce safe state
-    -- <https://jfmengels.net/single-out-elements-using-phantom-types/>
+      - <https://jfmengels.net/single-out-elements-using-phantom-types/>
+
+      - Add phantom type to `Pool`:
+
+            type Status
+                = AwaitingNextShot
+                | AwaitingRack
+                | AwaitingPlaceBallInHand
 
 
 # Init
 
-@docs Pool, eightBall
+@docs Pool, start
 
 
 # View
 
-@docs currentPlayer
-
 
 # Update
 
-@docs Action, update, nextAction
+@docs rack, ballPlacedInKitchen, playerShot
+
+
+## Events
+
+@docs cueHitBall
+
+
+## Balls
+
+@docs oneBall
+
+
+## Ruling
+
+@docs WhatHappened
+
+@docs Action, update, nextAction, rack
 
 -}
 
 import Time
 
 
+playerTookTurn : WhatHappened
+playerTookTurn =
+    start
+        |> rack (Time.millisToPosix 0)
+        |> ballPlacedInKitchen (Time.millisToPosix 0)
+        |> playerShot
+            [ twoBallsCollided (Time.millisToPosix 1)
+            ]
+
+
+
+-- test :  Pool state
+
+
+test whatHappened =
+    case playerTookTurn of
+        PlayersFault pool ->
+            Debug.todo " players fault"
+
+        NextShot _ ->
+            Debug.todo "handle NextTurn _"
+
+        Error _ ->
+            Debug.todo "handle Error"
+
+        GameOver _ ->
+            Debug.todo "handle GameOver _"
+
+
 
 -- Model
 
 
-type Pool player
-    = Pool (PoolData player)
+{-| This has details about the state of the game as it pertains to rules.
+This is what goes into your `Model`.
+-}
+type Pool state
+    = Pool PoolData
 
 
-type alias PoolData player =
-    { players : List player -- TODO: Consider whether to compare/enforce unique.
-    , events : List (Event player)
-    , gameFormat : GameFormat
+type alias PoolData =
+    { events : List EventData
     }
 
 
-type Event player
-    = Event
-        { when : Time.Posix
-        , type_ : EventType player
-        }
+
+-- type Status =
 
 
-type EventType player
-    = EventAction (Action player)
-    | EventRuling (Ruling player)
+type alias EventData =
+    { when : Time.Posix
+    , event : InternalEvent
+    }
 
 
-type Ruling player
+
+-- type EventType
+--     = EventAction Action
+--     | EventRuling Ruling
+
+
+type Ruling
     = Foul -- FoulType
-    | Win player
+    | Win
 
 
 type
@@ -72,120 +135,284 @@ type
 
 
 type Ball
-    = Numbered Int
-    | Cue
+    = OneBall
+    | TwoBall
+    | ThreeBall
+
+
+oneBall : Ball
+oneBall =
+    OneBall
+
+
+type Cue
+    = Cue
 
 
 
 -- Init
 
 
-{-| Start a game of 8-ball.
+{-| Start a game.
 -}
-eightBall : player -> player -> Pool player
-eightBall player1 player2 =
+start : Pool AwaitingRack
+start =
     Pool
-        { players = [ player1, player2 ]
-        , events = []
-        , gameFormat = Format8Ball
+        { events = []
         }
-
-
-
--- View
-
-
-currentPlayer : Pool player -> Maybe player
-currentPlayer (Pool { players }) =
-    List.head players
-
-
-{-| Possibly return invalid events too:
-
-    history :
-        Pool player
-        ->
-            { events : List Event player
-            , errors : List Event player
-            }
-
--}
-history : Pool player -> List (Event player)
-history (Pool { events }) =
-    events
 
 
 
 -- Update
 
 
-type Action player
-    = Rack (List Ball)
-    | PlaceBallInKitchen player
+type AwaitingRack
+    = AwaitingRack
 
 
-update : List ( Time.Posix, Action player ) -> Pool player -> Pool player
-update actions (Pool data) =
-    Debug.todo "update with new events"
-
-
-{-|
-
-1.  Possibly use `Valid` to ensure valid state:
-
-    nextAction : Pool player -> Result Error ( Valid (Pool player), Action player )
-
-    update : List ( Time.Posix, Action player ) -> Valid (Pool player) -> Pool player
-
-2.  Would there ever be multiple potential actions?
-
--}
-nextAction : Pool player -> Maybe (Action player)
-nextAction (Pool data) =
-    case data.gameFormat of
-        Format8Ball ->
-            nextAction8Ball data
+type AwaitingNextShot
+    = AwaitingNextShot
 
 
 
--- 8 ball
+-- type AwaitingCollisions
+--     = AwaitingCollisions
 
 
-nextAction8Ball : PoolData player -> Maybe (Action player)
-nextAction8Ball data =
-    case data.events of
+type AwaitingBallInHand
+    = AwaitingBallInHand
+
+
+type AwaitingBallInKitchen
+    = AwaitingBallInKitchen
+
+
+type AwaitingNewGame
+    = AwaitingNewGame
+
+
+
+-- type Action state
+--     = Action
+
+
+rack : Time.Posix -> Pool AwaitingRack -> Pool AwaitingBallInKitchen
+rack when (Pool data) =
+    Pool
+        { data
+            | events =
+                data.events
+                    ++ [ { when = when
+                         , event = Racked
+                         }
+                       ]
+        }
+
+
+type InternalEvent
+    = Racked
+    | BallPlacedInKitchen
+    | CueHitBall Ball
+
+
+ballPlacedInKitchen : Time.Posix -> Pool AwaitingBallInKitchen -> Pool AwaitingNextShot
+ballPlacedInKitchen when (Pool data) =
+    Pool
+        { data
+            | events =
+                data.events
+                    ++ [ { when = when
+                         , event = BallPlacedInKitchen
+                         }
+                       ]
+        }
+
+
+type Event
+    = Event EventData
+
+
+type WhatHappened
+    = PlayersFault (Pool AwaitingBallInHand)
+    | NextShot (Pool AwaitingNextShot)
+      -- | NextTurn (Pool AwaitingNextTurn)
+    | GameOver (Pool AwaitingNewGame) -- { winner : Int, loser: Int }
+    | Error String
+
+
+cueHitBall : Time.Posix -> Ball -> Event
+cueHitBall when ball =
+    Event
+        { when = when
+        , event = CueHitBall ball
+        }
+
+
+twoBallsCollided : Time.Posix -> Event
+twoBallsCollided =
+    Debug.todo ""
+
+
+ballTouchedTheWall : Time.Posix -> Event
+ballTouchedTheWall =
+    Debug.todo ""
+
+
+ballFellInPocket : Time.Posix -> Event
+ballFellInPocket =
+    Debug.todo ""
+
+
+playerShot : List Event -> Pool AwaitingNextShot -> WhatHappened
+playerShot events (Pool data) =
+    let
+        allEventDataSorted =
+            data.events
+                ++ List.map (\(Event eventData) -> eventData) events
+                |> List.sortWith eventTimeComparison
+
+        newPoolData =
+            { data
+                | events = allEventDataSorted
+            }
+    in
+    case allEventDataSorted |> List.map .event of
         [] ->
-            -- First event, let's rack!
-            Just <|
-                Rack
-                    [ Numbered 1
-                    , Numbered 2
-                    , Numbered 3
-                    , Numbered 4
-                    , Numbered 5
-                    , Numbered 6
-                    , Numbered 7
-                    , Numbered 8
-                    , Numbered 9
-                    , Numbered 10
-                    , Numbered 11
-                    , Numbered 12
-                    , Numbered 13
-                    , Numbered 14
-                    , Numbered 15
-                    ]
+            Error "Should already be racked and ball placed."
 
-        _ ->
-            Debug.todo "determine next action"
+        [ singleEvent ] ->
+            Error "Should already be racked and ball placed."
+
+        Racked :: BallPlacedInKitchen :: [] ->
+            NextShot <|
+                Pool newPoolData
+
+        Racked :: BallPlacedInKitchen :: otherEvents ->
+            checkEvents Racked BallPlacedInKitchen otherEvents newPoolData
+
+        firstEvent :: secondEvent :: otherEvents ->
+            Error "Should be racked, then ball in kitchen first"
 
 
+{-| May need to check for equal times and put things like Racked before BallPlacedInKitchen and so on.
+-}
+eventTimeComparison : EventData -> EventData -> Order
+eventTimeComparison eventData1 eventData2 =
+    compare
+        (Time.toMillis Time.utc eventData1.when)
+        (Time.toMillis Time.utc eventData2.when)
 
+
+checkEvents : InternalEvent -> InternalEvent -> List InternalEvent -> PoolData -> WhatHappened
+checkEvents eventData1 eventData2 remainingEventData poolData =
+    case ( eventData1, eventData2, remainingEventData ) of
+        ( Racked, BallPlacedInKitchen, [] ) ->
+            NextShot <|
+                Pool poolData
+
+        ( Racked, BallPlacedInKitchen, anotherEvent :: otherEvents ) ->
+            checkEvents eventData2 anotherEvent otherEvents poolData
+
+        ( BallPlacedInKitchen, CueHitBall ball, _ ) ->
+            GameOver (Pool poolData)
+
+        ( BallPlacedInKitchen, Racked, _ ) ->
+            Error "Nonsense, rack before placing ball in the kitchen!"
+
+        ( BallPlacedInKitchen, BallPlacedInKitchen, _ ) ->
+            -- Once ball is placed in the kitchen, it cannot be moved until a shot is taken.
+            PlayersFault (Pool poolData)
+
+        ( CueHitBall _, _, _ ) ->
+            Debug.todo "handle ( CueHitBall _, _, _ )"
+
+        ( Racked, _, _ ) ->
+            Debug.todo "handle ( Racked, Racked, _ )"
+
+
+stopTheGame : Pool { a | gameStoppable : Bool } -> Pool AwaitingRack
+stopTheGame =
+    Debug.todo ""
+
+
+{-| These are the available actions a game engine can use to update a pool game.
+-}
+
+
+
+-- type Action
+--     = -- Table actions
+--       -- | BallOffTable Ball
+--       -- | BallToBall Ball Ball (List Ball)
+--       -- | BallToWall Ball Wall
+--       -- | BallToPocket Ball Pocket
+--       Rack Rack
+--       -- Player actions
+--       -- | CallShot
+--       -- | PlaceBallInHand
+--       -- | Shoot (List Ball)  -- Is this even necessary with `BallToBall`? Maybe in a `Status` type.
+--     | PlaceBallInKitchen
+
+
+type Rack
+    = Rack_ (List Ball)
+
+
+
+-- {-| Where new events can be sent to update based on the game rules.
+--
+--     game =
+--         Pool.eightBall Player1 Player2
+--
+--     rackedGame =
+--         game
+--             |> Pool.update
+--                 [ ( Time.millisToPosix 0
+--                   , Pool.rack game
+--                   )
+--                 ]
+--
+-- -}
+-- update : List ( Time.Posix, Action ) -> Pool -> Pool
+-- update actions (Pool data) =
+--     Debug.todo "update with new events"
+-- {-|
+--
+-- 1.  Possibly use `Valid` to ensure valid state:
+--
+--     nextAction : Pool -> Result Error ( Valid (Pool ), Valid (Action ) )
+--
+--     update : List ( Time.Posix, Valid (Action ) ) -> Valid (Pool ) -> Pool
+--
+-- 2.  Would there ever be multiple potential actions?
+--
+-- -}
+-- nextAction : Pool -> Maybe Action
+-- nextAction (Pool data) =
+--     case data.gameFormat of
+--         Format8Ball ->
+--             nextAction8Ball data
+-- 8 ball
+-- nextAction8Ball : PoolData -> Maybe Action
+-- nextAction8Ball data =
+--     case data.events of
+--         [] ->
+--             -- First event, let's rack!
+--             Just <|
+--                 rack8Ball
+--
+--         _ ->
+--             Debug.todo "determine next action"
 -- [ firstEvent ] ->
 --     data.events
 --         |> List.sortBy (\(Event { when }) -> Time.posixToMillis when)
 --         |> List.foldl nextAction8BallStepper firstEvent
-
-
-nextAction8BallStepper : Event player -> Maybe (Action player) -> Maybe (Action player)
-nextAction8BallStepper playerEvent maybeAction =
-    Nothing
+-- nextAction8BallStepper : Event -> Maybe Action -> Maybe Action
+-- nextAction8BallStepper playerEvent maybeAction =
+--     Nothing
+-- View
+-- {-| Show the current player, if there is one.
+-- -}
+-- currentPlayer : Pool -> Maybe
+-- currentPlayer (Pool { players }) =
+--     List.head players
--- tests/PoolTests.elm
+++ tests/PoolTests.elm
@@ -10,81 +10,79 @@ import Time
 suite : Test
 suite =
     describe "Pool"
-        [ describe "Format8Ball"
-            [ describe "init"
-                [ describe "eightball"
-                    [ test "next action is to rack all the balls"
-                        (\_ ->
-                            let
-                                nextAction =
-                                    Pool.eightBall Player1 Player2
-                                        |> Pool.nextAction
-                            in
-                            case nextAction of
-                                Just (Pool.Rack balls) ->
-                                    balls
-                                        |> Expect.equalLists
-                                            [ Pool.Numbered 1
-                                            , Pool.Numbered 2
-                                            , Pool.Numbered 3
-                                            , Pool.Numbered 4
-                                            , Pool.Numbered 5
-                                            , Pool.Numbered 6
-                                            , Pool.Numbered 7
-                                            , Pool.Numbered 8
-                                            , Pool.Numbered 9
-                                            , Pool.Numbered 10
-                                            , Pool.Numbered 11
-                                            , Pool.Numbered 12
-                                            , Pool.Numbered 13
-                                            , Pool.Numbered 14
-                                            , Pool.Numbered 15
-                                            ]
+        [ describe "init"
+            [ skip <|
+                test "next action is to rack all the balls"
+                    (\_ ->
+                        let
+                            nextAction =
+                                Pool.start
+                                    |> Pool.rack (Time.millisToPosix 0)
+                        in
+                        Expect.fail "How do I test this?"
+                    )
+            ]
+        , describe "update"
+            [ describe "playerShot"
+                [ test "after player shoots cue into another ball, player wins!"
+                    (\_ ->
+                        let
+                            nextAction =
+                                Pool.start
+                                    |> Pool.rack (Time.millisToPosix 0)
+                                    |> Pool.ballPlacedInKitchen (Time.millisToPosix 0)
+                                    |> Pool.playerShot
+                                        [ Pool.cueHitBall (Time.millisToPosix 1) Pool.oneBall
+                                        ]
+                        in
+                        case nextAction of
+                            Pool.GameOver _ ->
+                                Expect.pass
 
-                                _ ->
-                                    Expect.fail "Should be Rack [1-15]"
-                        )
-                    ]
-                ]
-            , describe "update"
-                [ describe "nextAction"
-                    [ test "after rack next action is to place ball in the kitchen"
-                        (\_ ->
-                            let
-                                nextAction =
-                                    Pool.eightBall Player1 Player2
-                                        |> Pool.update
-                                            [ ( Time.millisToPosix 0
-                                              , Pool.Rack
-                                                    [ Pool.Numbered 1
-                                                    , Pool.Numbered 2
-                                                    , Pool.Numbered 3
-                                                    , Pool.Numbered 4
-                                                    , Pool.Numbered 5
-                                                    , Pool.Numbered 6
-                                                    , Pool.Numbered 7
-                                                    , Pool.Numbered 8
-                                                    , Pool.Numbered 9
-                                                    , Pool.Numbered 10
-                                                    , Pool.Numbered 11
-                                                    , Pool.Numbered 12
-                                                    , Pool.Numbered 13
-                                                    , Pool.Numbered 14
-                                                    , Pool.Numbered 15
-                                                    ]
-                                              )
-                                            ]
-                                        |> Pool.nextAction
-                            in
-                            case nextAction of
-                                Just (Pool.PlaceBallInKitchen player) ->
-                                    player
-                                        |> Expect.equal Player1
+                            other ->
+                                Expect.fail <|
+                                    "Should be Pool.GameOver, but found this instead:\n"
+                                        ++ Debug.toString other
+                    )
+                , test "no events sent, still current player's turn"
+                    (\_ ->
+                        let
+                            nextAction =
+                                Pool.start
+                                    |> Pool.rack (Time.millisToPosix 0)
+                                    |> Pool.ballPlacedInKitchen (Time.millisToPosix 0)
+                                    |> Pool.playerShot []
+                        in
+                        case nextAction of
+                            Pool.NextShot _ ->
+                                -- TODO: Inspect current player, should still be player 1.
+                                Expect.pass
 
-                                _ ->
-                                    Expect.fail "Should be PlaceBallInKitchen"
-                        )
-                    ]
+                            other ->
+                                Expect.fail <|
+                                    "Should be Pool.NextShot, but found this instead:\n"
+                                        ++ Debug.toString other
+                    )
+                , test "after player shoots cue hits nothing, next players turn"
+                    (\_ ->
+                        let
+                            nextAction =
+                                Pool.start
+                                    |> Pool.rack (Time.millisToPosix 0)
+                                    |> Pool.ballPlacedInKitchen (Time.millisToPosix 0)
+                                    |> Pool.playerShot
+                                        []
+                        in
+                        case nextAction of
+                            Pool.NextShot _ ->
+                                -- TODO: Inspect current player, should be player 2.
+                                Expect.pass
+
+                            other ->
+                                Expect.fail <|
+                                    "Should be Pool.NextShot, but found this instead:\n"
+                                        ++ Debug.toString other
+                    )
                 ]
             ]
         ]
